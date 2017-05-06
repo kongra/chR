23b18ad640550d9ae710d81aeb6842c0d5c2a326
Konrad Grzanek <kongra@gmail.com>
2017-05-06 22:38:21 +0200

    Added basic mechanisms for reporting ch(eck) problems. In progress ...

--- R/core.R
+++ R/core.R
@@ -1,2 +1,11 @@
 # Copyright (c) Konrad Grzanek
 # Created: 2017-05-05
+
+chMessage <- function(x, checkName) {
+  r <- paste(capture.output(str(x)), collapse = "\n")
+  paste0("The following object violated ch(eck) ", checkName, "\n", r)
+}
+
+#' Tells if x is a scalar value
+#' @export
+isScalar <- function(x) is.atomic(x) && length(x) == 1L
