28de13605ba2cf02c9a765f895f4b8d85202ad91
Konrad Grzanek <kongra@gmail.com>
2017-05-08 16:04:45 +0200

    Added the checks registry.

--- R/core.R
+++ R/core.R
@@ -52,19 +52,59 @@ chOr <- function(...) {
   })
 }
 
-# CH(ECK)S
+# ESSENTIAL CH(ECK)S
 
 #' \code{is.null} ch(eck)
 #' @export
 chUnit <- chP(is.null)
 
+#' Scalar \code{is.atomic} & \code{length == 1L} value ch(eck)
+#' @export
+chScalar <- chP(function(x) is.atomic(x) && length(x) == 1L)
+
+#' \code{is.character} ch(eck)
+#' @export
+chStrings <- chP(is.character)
+
+#' \code{chScalar} & \code{chStrings} ch(eck)
+#' @export
+chString <- chAnd(chScalar, chStrings)
+
+#' \code{is.function} ch(eck)
+#' @export
+chFun <- chP(is.function)
+
+# REGISTRY
+
+checksRegistry <- list()
+
+#' Registeres the ch(eck) using an optional name (ch(eck) name by default)
+#' @export
+chReg <- function(ch, name = NA) chUnit({ # BEWARE: THREAD UNSAFE
+  if (is.na(name)) {
+    name <- as.character(substitute(ch))
+  }
+  checksRegistry[[chString(name)]] <<- chFun(ch)
+  NULL
+})
+
+chReg(chUnit)
+chReg(chScalar)
+chReg(chString)
+chReg(chStrings)
+chReg(chFun)
+
+# REST OF THE CH(ECK)S
+
 #' \code{!is.null} ch(eck)
 #' @export
 chSome <- chP(function(x) !is.null(x))
+chReg(chSome)
 
 #' \code{is.na} ch(eck)
 #' @export
 chNA <- chP(is.na)
+chReg(chNA)
 
 #' Either ch(eck) where the left and right types are expressed by checks
 #' cl and cr.
@@ -75,145 +115,162 @@ chEither <- function(cl, cr, x, asPred = FALSE) chOr(cl, cr)(x, asPred)
 #' @export
 chMaybe <- function(c, x, asPred = FALSE) chEither(chUnit, c, x, asPred)
 
-#' Scalar \code{is.atomic} & \code{length == 1L} value ch(eck)
-#' @export
-chScalar <- chP(function(x) is.atomic(x) && length(x) == 1L)
-
 #' \code{is.logical} ch(eck)
 #' @export
 chBools <- chP(is.logical)
+chReg(chBools)
 
 #' \code{chScalar} & \code{chBools} ch(eck)
 #' @export
 chBool <- chAnd(chScalar, chBools)
+chReg(chBool)
 
 #' \code{is.integer} ch(eck)
 #' @export
 chInts <- chP(is.integer)
+chReg(chInts)
 
 #' \code{chScalar} & \code{chInts} ch(eck)
 #' @export
 chInt <- chAnd(chScalar, chInts)
+chReg(chInt)
 
 #' \code{is.double} ch(eck)
 #' @export
 chDoubles <- chP(is.double)
+chReg(chDoubles)
 
 #' \code{chScalar} & \code{chDoubles} ch(eck)
 #' @export
 chDouble <- chAnd(chScalar, chDoubles)
+chReg(chDouble)
 
 #' \code{is.complex} ch(eck)
 #' @export
 chComplexes <- chP(is.complex)
+chReg(chComplexes)
 
 #' \code{chScalar} & \code{chComplexes} ch(eck)
 #' @export
 chComplex <- chAnd(chScalar, chComplexes)
+chReg(chComplex)
 
 #' \code{is.numeric} ch(eck)
 #' @export
 chNumerics <- chP(is.numeric)
+chReg(chNumerics)
 
 #' \code{chScalar} & \code{chNumerics} ch(eck)
 #' @export
 chNumeric  <- chAnd(chScalar, chNumerics)
+chReg(chNumeric)
 
 #' \code{is.infinite} ch(eck)
 #' @export
 chInfinites <- chP(is.infinite)
+chReg(chInfinites)
 
 #' \code{chScalar} & \code{chInfinites} ch(eck)
 #' @export
 chInfinite  <- chAnd(chScalar, chInfinites)
+chReg(chInfinite)
 
 #' \code{is.Finite} ch(eck)
 #' @export
 chFinites <- chP(is.finite)
+chReg(chFinites)
 
 #' \code{chScalar} & \code{chFinites} ch(eck)
 #' @export
 chFinite  <- chAnd(chScalar, chFinites)
+chReg(chFinite)
 
 #' \code{chInt} & > 0 ch(eck)
 #' @export
 chPosInt <- chAnd(chInt, chP(function (x) x > 0))
+chReg(chPosInt)
 
 #' \code{chInt} & >= 0 ch(eck)
 #' @export
 chNatInt <- chAnd(chInt, chP(function (x) x >= 0))
-
-#' \code{is.character} ch(eck)
-#' @export
-chStrings <- chP(is.character)
-
-#' \code{chScalar} & \code{chStrings} ch(eck)
-#' @export
-chString <- chAnd(chScalar, chStrings)
+chReg(chNatInt)
 
 #' \code{is.list} ch(eck)
 #' @export
 chList <- chP(is.list)
+chReg(chList)
 
 #' \code{is.vector} ch(eck)
 #' @export
 chVector <- chP(is.vector)
+chReg(chVector)
 
 #' \code{is.factor} ch(eck)
 #' @export
 chFactor <- chP(is.factor)
+chReg(chFactor)
 
 #' \code{is.data.frame} ch(eck)
 #' @export
 chDF <- chP(is.data.frame)
+chReg(chDF)
 
 #' \code{data.table::is.data.table} ch(eck)
 #' @export
 chDT <- chP(data.table::is.data.table)
+chReg(chDT)
 
 #' \code{ggplot2::is.ggplot} ch(eck)
 #' @export
 chGgplot <- chP(ggplot2::is.ggplot)
+chReg(chGgplot)
 
 #' \code{tibble::is.tibble} ch(eck)
 #' @export
 chTibble <- chP(tibble::is.tibble)
+chReg(chTibble)
 
 #' \code{is.array} ch(eck)
 #' @export
 chArray <- chP(is.array)
+chReg(chArray)
 
 #' \code{is.atomic} ch(eck)
 #' @export
 chAtomic <- chP(is.atomic)
-
-#' \code{is.function} ch(eck)
-#' @export
-chFun <- chP(is.function)
+chReg(chAtomic)
 
 #' \code{is.matrix} ch(eck)
 #' @export
 chMatrix <- chP(is.matrix)
+chReg(chMatrix)
 
 #' \code{is.table} ch(eck)
 #' @export
 chTable <- chP(is.table)
+chReg(chTable)
 
 #' \code{is.environment} ch(eck)
 #' @export
 chEnv <- chP(is.environment)
+chReg(chEnv)
 
 #' \code{is.call} ch(eck)
 #' @export
 chCall <- chP(is.call)
+chReg(chCall)
 
 #' \code{is.expression} ch(eck)
 #' @export
 chExpr <- chP(is.expression)
+chReg(chExpr)
 
 #' \code{is.symbol} ch(eck)
 #' @export
 chSymbol <- chP(is.symbol)
+chReg(chSymbol)
+
+# QUERYING THE REGISTRY
 
 # TAGGED (CONSTRAINED) VALUES
 
