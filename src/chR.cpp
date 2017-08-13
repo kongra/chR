2e559802bcc072769d26b0a83a223340b8b97fd8
kongra <kongra@gmail.com>
2017-08-13 13:15:26 +0200

    Routines exported properly.

--- src/chR.cpp
+++ src/chR.cpp
@@ -19,16 +19,25 @@ static inline bool everyInVector(const V xs, const F&& pred) {
 // INTEGRALS
 //
 
+//' Returns true iff all the xs are positive
+//' @param xs
+//' @export
 // [[Rcpp::export]]
 bool arePosInts(const IntegerVector xs) {
   return everyInVector(xs, [](int n) { return n > 0; });
 }
 
+//' Returns true iff all the xs are negative
+//' @param xs
+//' @export
 // [[Rcpp::export]]
 bool areNegInts(const IntegerVector xs) {
   return everyInVector(xs, [](int n) { return n < 0; });
 }
 
+//' Returns true iff all the xs are naturals (>= 0)
+//' @param xs
+//' @export
 // [[Rcpp::export]]
 bool areNatInts(const IntegerVector xs) {
   return everyInVector(xs, [](int n) { return n >= 0; });
